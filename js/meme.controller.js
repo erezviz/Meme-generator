f9bcdc7f3760aa2d1c3d0d4f34da447265333ab7
erezviz <erezviz@gmail.com>
2022-04-13 16:34:25 +0300

    For google forms CA

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -6,11 +6,11 @@ let gStartPos
 const gTouchEvs = ['touchstart', 'touchmove', 'touchend']
 
 function onInit() {
-    gElCanvas = document.querySelector('#meme-canvas')
-    gCtx = gElCanvas.getContext('2d')
-        // addEventListeners()
-    drawText('test', gElCanvas.width - 50, gElCanvas.height - 50)
-    renderMeme()
+    // renderMeme()
+    // gElCanvas = document.querySelector('#meme-canvas')
+    // gCtx = gElCanvas.getContext('2d')
+    // addEventListeners()
+    // drawText('test', gElCanvas.width - 50, gElCanvas.height - 50)
 }
 
 // function addEventListeners() {
@@ -19,38 +19,63 @@ function onInit() {
 // }
 
 function renderMeme() {
-    const meme = getMemeById(1)
-    drawMeme(meme)
+    const currMeme = getMeme()
+
+    let strHtml = `<canvas id="meme-canvas" width="500" height="500"></canvas>
+    <div class="control-box">
+        <label for="txt-line">
+
+            <input oninput="onTxtChange(this.value)" type="text" placeholder="Write meme here">
+        </label>
+        <button class="move-left"></button>
+        <button class="move-up"></button>
+        <button class="move-down"></button>
+        <button class="move-right"></button>
+    </div>`
+    const editor = document.querySelector('.meme-editor')
+    editor.innerHTML = strHtml
+
+
+    if (editor.classList.contains('hide')) {
+
+        editor.classList.remove('hide')
+        editor.classList.add('flex')
+
+    }
+
+    drawMeme(currMeme)
 
 
 
 }
 
-console.dir(gElCanvas)
+
 
 function drawMeme(meme) {
-    console.log('in draw meme');
-    let img = new Image();
-    img.src = meme.url;
+
+    let img = new Image()
+    img.src = meme.img.url
     img.onload = () => {
         gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height);
-        drawText('Meme Line 1', gElCanvas.width / 2, gElCanvas.height / 10)
-    };
+        drawText(meme.info, gElCanvas.width / 2, gElCanvas.height / 10)
+    }
 
 }
 
+function drawText(info, x, y) {
 
-function drawText(txt, x, y) {
+    gCtx.font = `${info.size}px Impact`
+    gCtx.textBaseline = 'middle'
+    gCtx.textAlign = info.align
+    gCtx.lineWidth = 2
+    gCtx.fillStyle = 'white'
+    gCtx.font = '50px david'
+    gCtx.strokeStyle = info.color
+    gCtx.strokeText(info.txt, x, y)
+}
 
 
-    gCtx.font = '48px serif';
-    // gCtx.fillText(txt, x, y);
-    gCtx.textBaseline = 'middle';
-    gCtx.textAlign = 'center';
-    gCtx.lineWidth = 2;
-    gCtx.fillStyle = 'white';
-    gCtx.font = '50px david';
-    // gCtx.fillText(txt, x, y);
-    gCtx.strokeStyle = 'white';
-    gCtx.strokeText(txt, x, y);
+function onTxtChange(val) {
+    setLineTxt(val)
+    renderMeme()
 }
--- js/meme.service.js
+++ js/meme.service.js
@@ -1,12 +1,28 @@
 'use strict'
 const IMG_KEY = 'imagesDB'
-let gImgs = [{ id: 1, url: 'meme-imgs-square/1.jpg', keywords: ['funny', 'cat'] }]
-let gMeme = {
-    selectedImgId: 1,
-    selectedLineIdx: 0
-}
-const ImageCount = 18
 
+let gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
+let gImgs = [
+    { id: 1, url: 'meme-imgs-square/1.jpg', keywords: ['funny', 'cat'] },
+    { id: 2, url: 'meme-imgs-square/2.jpg', keywords: ['funny', 'cat'] },
+    { id: 3, url: 'meme-imgs-square/3.jpg', keywords: ['funny', 'cat'] },
+    { id: 4, url: 'meme-imgs-square/4.jpg', keywords: ['funny', 'cat'] }
+]
+let gMeme = {
+    selectedImgId: 3,
+    selectedLineIdx: 0,
+    lines: [{
+        txt: 'I sometimes eat Falafel',
+        size: 20,
+        align: 'center',
+        color: 'red'
+    }]
+}
+
+function setLineTxt(newTxt) {
+    const meme = getMeme()
+    meme.info.txt = newTxt
+}
 // function createImages(){
 //     gImgs = loadFromStorage(IMG_KEY)
 //     if(!gImgs){
@@ -20,5 +36,12 @@ const ImageCount = 18
 // }
 
 function getMeme() {
-    return gMeme
+    return {
+        info: gMeme.lines[gMeme.selectedLineIdx],
+        img: _getImgById(gMeme.selectedImgId)
+    }
+}
+
+function _getImgById(imgId) {
+    return gImgs.find(img => img.id === imgId)
 }
